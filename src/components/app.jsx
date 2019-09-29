63d524f554e27f061bce075cb52cd9ce77be22a4
Christopher  Tso <christophertso@Christophers-iMac.local>
2019-09-29 14:26:56 -0500

    Added menu bar

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,1 +1,50 @@
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+});
+
+
+class App extends React.Component{
+    constructor(props){
+        super(props); 
+        this.state = {
+
+        }
+    }
+    render(){
+        const {classes} = this.props;
+        return (
+            <div> 
+              <AppBar position="static">
+                <Toolbar>
+                  <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                    <MenuIcon />
+                  </IconButton>
+                  <Typography variant="h6" className={classes.title}>
+                    Movie List 
+                  </Typography>
+                  <Button color="inherit">Login</Button>
+                  </Toolbar>
+              </AppBar>
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(App);
