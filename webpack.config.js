a431d811913f0cd96986fdaf86b104fcac16060d
Christopher  Tso <christophertso@Christophers-iMac.local>
2019-09-29 12:58:37 -0500

    Created webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,22 @@
 
+module.exports = {
+    entry: __dirname + '/src/index.js', 
+    modules: {
+        rules: [
+            {
+                test: [/\.jsx$/],
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: ['@babel/preset-react', '@babel/preset-env']
+                  }
+                }
+            }
+        ]
+    }, 
+    output: {
+        filename: 'bundle.js', 
+        path: __dirname + '/public'
+    }
+}
