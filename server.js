d7254eccc453e10349f9920a8c3b6c915597d162
Christopher  Tso <christophertso@Christophers-iMac.local>
2019-09-29 14:26:02 -0500

    Served static files

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express =  require('express')
 var app = express(); 
 
 var PORT = 3000; 
+app.use(express.static(__dirname + '/public'))
 app.listen(PORT, () => {
     console.log('Listening at 3000')
 })
